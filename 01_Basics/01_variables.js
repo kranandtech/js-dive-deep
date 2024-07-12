389602f63870b7e13438f625a46f3a413ebaf77b
Anand Kumar <anandkumarmca6@gmail.com>
2024-07-12 13:28:01 +0000

    datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 12345;
+let accountEmail = "acc@gmail.com";
+var accountPassword = "12345";
+accountCity = "Patna"
+
+//accountId = 123456; // Not allowed
+/*
+ not to use var due to issue in fucntional scope and block scope
+*/
+
+accountEmail = "ak@gmail.com";
+accountPassword = "1234";
+accountCity = "Delhi"
+console.table([accountId,accountEmail,accountPassword,accountCity]);
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict" ;// treat all js code as newer versions
+
+let userName = "ram";
+let age =19;
+let isLoggedIn = false;
+
+// number
+// bigint
+// string => "" or ''
+// boolean => true/false
+// null
+// undefined
+// symbol 
+
+// object
+
+console.table([ typeof userName, typeof age,typeof isLoggedIn, typeof null,typeof undefined]);
