06ff6beca41d4fbf4099f134432397a29ed5bc3f
Anand Kumar <anandkumarmca6@gmail.com>
2024-07-13 07:42:31 +0000

    datatype updated

--- datatype_summary.js
+++ datatype_summary.js
@@ -26,3 +26,30 @@ const obj = {
 };
 console.log(typeof arr);
 console.log(typeof obj);
+console.log("*********************");
+// Stack(primitive) , Heap(Non-primitive)
+
+let myEmail1 = "ak@gmail.com";
+let myEmail2 = myEmail1;
+myEmail2 = "anand@gmail.com";
+console.table([myEmail1,myEmail2]);
+/*
+┌─────────┬───────────────────┐
+│ (index) │ Values            │
+├─────────┼───────────────────┤
+│ 0       │ 'ak@gmail.com'    │
+│ 1       │ 'anand@gmail.com' │
+└─────────┴───────────────────┘
+*/
+let userOne = {
+    email:"one@gmail.com"
+};
+let userTwo = userOne;
+userTwo.email = "two@gmail.com";
+console.table([userOne,userTwo]);
+/*┌─────────┬─────────────────┐
+│ (index) │ email           │
+├─────────┼─────────────────┤
+│ 0       │ 'two@gmail.com' │
+│ 1       │ 'two@gmail.com' │
+└─────────┴─────────────────┘*/
